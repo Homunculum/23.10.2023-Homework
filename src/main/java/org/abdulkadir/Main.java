6496ea26b81766593161036b8c7cd7e931866e01
homunculum <a.kadir.e.91@gmail.com>
2023-10-24 00:58:28 +0300

    23.10.2023 first lesson first homework

--- src/main/java/org/abdulkadir/Main.java
+++ src/main/java/org/abdulkadir/Main.java
@@ -1,19 +1,33 @@
 package org.abdulkadir;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+
+        String ortaMetin = "İlginizi çekebilir";
+        String altMetin  = " Vade süresi";
+        System.out.printf(ortaMetin);
+
+        int vade = 12;
+
+        double dolarDun = 18.14;
+        double dolarBugun = 18.10;
+
+        boolean dolarDustuMu = false;
+        String okYonu = "down.svg";
+        if (dolarBugun<dolarDun){
+            okYonu = "down.svg";
+            System.out.println(okYonu);
+        } else {
+            okYonu = "up.svg";
+            System.out.println(okYonu);
+        }
+        String[] krediler = {"Hızlı Kredi", "Maaşını Halkbanktan Alanlar", "Mutlu Kredi"};
+
+        for (int i = 0; i < krediler.length; i++){
+            System.out.println(krediler[i]);
         }
     }
 }
